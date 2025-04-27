09dafaeb0668f3660cce6ec44e097ee8fa79ce41
Marcus Lorang <lorangmarcus@gmail.com>
2025-04-27 12:04:33 +0200

    feat: update site configuration and homepage
    - updated readme.md
    - added site in astro.config.mjs
    - improved index.astro homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://marcuslorang.com/',
 	integrations: [react(), sitemap(), mdx()],
 })
